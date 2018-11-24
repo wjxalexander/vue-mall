502d3c74bc13b2c32643b10827bc2a7cac564e23
wjxalexander <wjxalexander@hotmail.com>
2018-11-24 18:17:57 +0800

    解决

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)//路由是一个个插件 使用
-// 数组配置
-let routers = [{
+// 数组配置 这类不能写routers？？？
+let routes = [{
   path:'/',
   component: require('./components/member.vue')
 }]
 //实例化router
 let router = new VueRouter({
-  routers//解构赋值
+  routes//解构赋值
 })
 //根组件注册
 new Vue({
   el: '#app',//这里是HTML的挂载点
   //注入路由
   router,
-  mounted(){
-    console.log('323')
-  }
+ 
 })
